aeab97396fd5cc4daf7dff5f6d945e3f7aa82cf5
xvasgit <erlanggapermana8279@gmail.com>
2026-06-09 14:21:32 +0700

    pretest m12

---
+++ modul12/pretest_m12/src/LibraryManager.java
@@ -0,0 +1,25 @@
+public class LibraryManager{
+    public void cariBuku(String judul){}
+    public void pinjamBuku(String judul){}
+}
+
+class kalkulatorDenda{
+    public void dendaTelat(LibraryManager library){}
+}
+
+class cetakStrukDenda{
+    public void strukDenda(kalkulatorDenda calcDenda){}
+    public void strukDendaTabel(kalkulatorDenda calcDenda){}
+}
+
+
+// public class LibraryManager {
+//     public void cariBuku(String judul){}
+//     public void pinjamBuku(){}
+//     public void dendaTelat(){}
+//     public void strukDenda(){}
+//     public void strukDendaTabel(){}
+
+// }
+
+
